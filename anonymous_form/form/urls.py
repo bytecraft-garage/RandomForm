86c4b18be8425f5e2acffbf9cdb47fe400c83c74
Abijith <abijithka02@gmail.com>
2024-07-29 21:49:13 +0530

    create the admin panel and the user-interface to chat anonymously and controled by the admin end

--- anonymous_form/form/urls.py
+++ anonymous_form/form/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name ="home")
+    path('', views.home, name ="home"),
+    path('panel/',views.panel, name = "panel"),
+    path('delete/<id>',views.delete, name ="delete")
 ]
--- anonymous_form/form/views.py
+++ anonymous_form/form/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import querys
 
 # Create your views here.
 def home(request):
+    
+    if request.POST:
+        query = request.POST['text']
+        data = querys.objects.create(questions = query)
+        data.save()
+        print('saved Successfully')
     return render(request,'form.html')
+
+def panel(request):
+    data = querys.objects.all()
+    context = {'data' : data}
+    return render(request,'panel.html',context)
+
+def delete(request,id):
+    data = querys.objects.get(id = id)
+    data.delete()
+    data = querys.objects.all()
+    return render(request,'panel.html',{'data':data})
