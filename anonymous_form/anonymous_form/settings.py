afa213867ed60ce2e623583150d68f3c33bc4f14
Abijith <abijithka02@gmail.com>
2024-07-30 15:55:27 +0530

    Hosted contents on the pythonanywhere

--- anonymous_form/anonymous_form/settings.py
+++ anonymous_form/anonymous_form/settings.py
@@ -76,16 +76,25 @@ WSGI_APPLICATION = 'anonymous_form.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'random_form',
+#         'USER': 'postgres',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'random_form',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 
